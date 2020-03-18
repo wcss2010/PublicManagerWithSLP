7c70ff9611d83503607a2ba041f1875c989b3f5c
wcss <wcss2020@gmail.com>
2020-03-18 18:10:28 +0800

    ...

--- Code/PublicManager/PublicManager/WelcomeForm.Designer.cs
+++ Code/PublicManager/PublicManager/WelcomeForm.Designer.cs
@@ -30,10 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WelcomeForm));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnToA = new System.Windows.Forms.Button();
-            this.btnToB = new System.Windows.Forms.Button();
             this.lblUnitA = new System.Windows.Forms.Label();
-            this.btnSetUnitA = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,66 +45,25 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // btnToA
-            // 
-            this.btnToA.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnToA.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnToA.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnToA.ForeColor = System.Drawing.Color.Black;
-            this.btnToA.Location = new System.Drawing.Point(399, 363);
-            this.btnToA.Name = "btnToA";
-            this.btnToA.Size = new System.Drawing.Size(194, 47);
-            this.btnToA.TabIndex = 1;
-            this.btnToA.Text = "归口管理部门入口";
-            this.btnToA.UseVisualStyleBackColor = false;
-            this.btnToA.Click += new System.EventHandler(this.btnToA_Click);
-            // 
-            // btnToB
-            // 
-            this.btnToB.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnToB.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnToB.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnToB.ForeColor = System.Drawing.Color.Black;
-            this.btnToB.Location = new System.Drawing.Point(196, 363);
-            this.btnToB.Name = "btnToB";
-            this.btnToB.Size = new System.Drawing.Size(197, 47);
-            this.btnToB.TabIndex = 1;
-            this.btnToB.Text = "军队主管部门入口";
-            this.btnToB.UseVisualStyleBackColor = false;
-            this.btnToB.Click += new System.EventHandler(this.btnToB_Click);
-            // 
             // lblUnitA
             // 
-            this.lblUnitA.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lblUnitA.ForeColor = System.Drawing.Color.Red;
-            this.lblUnitA.Location = new System.Drawing.Point(443, 429);
+            this.lblUnitA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblUnitA.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblUnitA.ForeColor = System.Drawing.Color.Black;
+            this.lblUnitA.Location = new System.Drawing.Point(0, 329);
             this.lblUnitA.Name = "lblUnitA";
-            this.lblUnitA.Size = new System.Drawing.Size(368, 30);
+            this.lblUnitA.Size = new System.Drawing.Size(816, 58);
             this.lblUnitA.TabIndex = 0;
-            this.lblUnitA.Text = "...";
-            this.lblUnitA.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // btnSetUnitA
-            // 
-            this.btnSetUnitA.Location = new System.Drawing.Point(599, 380);
-            this.btnSetUnitA.Name = "btnSetUnitA";
-            this.btnSetUnitA.Size = new System.Drawing.Size(88, 30);
-            this.btnSetUnitA.TabIndex = 1;
-            this.btnSetUnitA.TabStop = true;
-            this.btnSetUnitA.Text = "设置所属单位";
-            this.btnSetUnitA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnSetUnitA.Click += new System.EventHandler(this.btnSetUnitA_Click);
+            this.lblUnitA.Text = "正在加载，请等待......";
+            this.lblUnitA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // WelcomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(816, 467);
+            this.ClientSize = new System.Drawing.Size(816, 387);
             this.Controls.Add(this.lblUnitA);
-            this.Controls.Add(this.btnToB);
-            this.Controls.Add(this.btnSetUnitA);
-            this.Controls.Add(this.btnToA);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -126,9 +82,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button btnToA;
-        private System.Windows.Forms.Button btnToB;
         private System.Windows.Forms.Label lblUnitA;
-        private System.Windows.Forms.LinkLabel btnSetUnitA;
     }
 }
--- Code/PublicManager/PublicManager/WelcomeForm.cs
+++ Code/PublicManager/PublicManager/WelcomeForm.cs
@@ -22,148 +22,15 @@ namespace PublicManager
         {
             base.OnLoad(e);
 
-            if (MainConfig.Config.StringDict.ContainsKey("当前所属单位名称"))
-            {
-                lblUnitA.Text = MainConfig.Config.StringDict["当前所属单位名称"];
-            }
-        }
-
-        private void btnToA_Click(object sender, EventArgs e)
-        {
-            switchToDB_A();
-
-            if (checkUnitA())
-            {
-                Top = Screen.PrimaryScreen.Bounds.Height * 5;
-                Modules.Module_A.ModuleMainForm form = new Modules.Module_A.ModuleMainForm();
-                form.FormClosing += form_FormClosing;
-                form.Show();
-            }
-        }
-
-        private void switchToDB_A()
-        {
-            #region 准备DB文件
-            string dir = Path.Combine(Application.StartupPath, Path.Combine("Data", "M_A"));
-            try
-            {
-                Directory.CreateDirectory(dir);
-            }
-            catch (Exception ex) { }
-            string dbFile = Path.Combine(dir, "static.db");
-            if (!File.Exists(dbFile))
-            {
-                File.Copy(Path.Combine(Application.StartupPath, "static.db"), dbFile, true);
-            }
-            #endregion
-
-            //初始化数据库
-            PublicManager.DB.ConnectionManager.Open("main", "Data Source=" + dbFile);
-        }
-
-        public static bool checkUnitA()
-        {
-            LocalUnit lu = ConnectionManager.Context.table("LocalUnit").select("*").getItem<LocalUnit>(new LocalUnit());
-            if (string.IsNullOrEmpty(lu.LocalUnitID))
-            {
-                return showUnitADialog();
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        public static bool showUnitADialog()
-        {
-            LocalUnit lu = ConnectionManager.Context.table("LocalUnit").select("*").getItem<LocalUnit>(new LocalUnit());
-            Modules.Module_A.DataManager.Forms.DutyUnitForm form = new Modules.Module_A.DataManager.Forms.DutyUnitForm();
-            form.Text = "设置所属单位";
-
-            if (form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                ConnectionManager.Context.table("LocalUnit").delete();
-
-                lu.LocalUnitID = Guid.NewGuid().ToString();
-                lu.LocalUnitName = form.SelectedItem;
-                lu.copyTo(ConnectionManager.Context.table("LocalUnit")).insert();
-
-                MainConfig.Config.StringDict["当前所属单位名称"] = form.SelectedItem;
-                MainConfig.saveConfig();
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            Top = Screen.PrimaryScreen.Bounds.Height * 5;
+            Modules.Module_A.ModuleMainForm form = new Modules.Module_A.ModuleMainForm();
+            form.FormClosing += form_FormClosing;
+            form.Show();
         }
 
         void form_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
-        }
-
-        private void btnToB_Click(object sender, EventArgs e)
-        {
-            switchToDB_B();
-
-            if (checkUnitB())
-            {
-                Top = Screen.PrimaryScreen.Bounds.Height * 5;
-                Modules.Module_B.ModuleMainForm form = new Modules.Module_B.ModuleMainForm();
-                form.FormClosing += form_FormClosing;
-                form.Show();
-            }
-        }
-
-        private void switchToDB_B()
-        {
-            #region 准备DB文件
-            string dir = Path.Combine(Application.StartupPath, Path.Combine("Data", "M_B"));
-            try
-            {
-                Directory.CreateDirectory(dir);
-            }
-            catch (Exception ex) { }
-            string dbFile = Path.Combine(dir, "static.db");
-            if (!File.Exists(dbFile))
-            {
-                File.Copy(Path.Combine(Application.StartupPath, "static.db"), dbFile, true);
-            }
-            #endregion
-
-            //初始化数据库
-            PublicManager.DB.ConnectionManager.Open("main", "Data Source=" + dbFile);
-        }
-
-        public static bool checkUnitB()
-        {
-            LocalUnit lu = ConnectionManager.Context.table("LocalUnit").select("*").getItem<LocalUnit>(new LocalUnit());
-            if (string.IsNullOrEmpty(lu.LocalUnitID))
-            {
-                lu.LocalUnitID = Guid.NewGuid().ToString();
-                lu.LocalUnitName = "军委机构";
-                lu.copyTo(ConnectionManager.Context.table("LocalUnit")).insert();
-            }
-
-            //Modules.Module_B.DataManager.Forms.DutyUnitForm form = new Modules.Module_B.DataManager.Forms.DutyUnitForm();
-
-            return true;
-        }
-
-        private void btnSetUnitA_Click(object sender, EventArgs e)
-        {
-            //切换数据库
-            switchToDB_A();
-
-            //显示对话框
-            showUnitADialog();
-
-            if (MainConfig.Config.StringDict.ContainsKey("当前所属单位名称"))
-            {
-                lblUnitA.Text = MainConfig.Config.StringDict["当前所属单位名称"];
-            }
         }
     }
 }
