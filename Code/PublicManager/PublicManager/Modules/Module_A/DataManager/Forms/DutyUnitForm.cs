1163928aa955b1335ad8735ecf96116f6fffbf70
wcss <wcss2020@gmail.com>
2020-03-18 16:45:16 +0800

    ...

---
+++ Code/PublicManager/PublicManager/Modules/Module_A/DataManager/Forms/DutyUnitForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PublicManager.Modules.Module_A.DataManager.Forms
+{
+    public partial class DutyUnitForm : Form
+    {
+        private List<string> dutyUnitToProfessonLinks;
+        /// <summary>
+        /// 责任单位(有类别的)
+        /// </summary>
+        public List<string> DutyUnitToProfessonLinks
+        {
+            get { return dutyUnitToProfessonLinks; }
+        }
+
+        public string SelectedItem
+        {
+            get
+            {
+                return (string)lbDutyUnits.SelectedItem;
+            }
+        }
+
+        public DutyUnitForm()
+        {
+            InitializeComponent();
+
+            //加载责任单位与专业类型映射选项
+            if (MainConfig.Config.ObjectDict.ContainsKey("责任单位与专业类别映射"))
+            {
+                try
+                {
+                    dutyUnitToProfessonLinks = new List<string>();
+                    Newtonsoft.Json.Linq.JArray teams = (Newtonsoft.Json.Linq.JArray)MainConfig.Config.ObjectDict["责任单位与专业类别映射"];
+                    foreach (string s in teams)
+                    {
+                        dutyUnitToProfessonLinks.Add(s);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.ToString());
+                }
+            }
+
+            //加载责任单位选项
+            if (MainConfig.Config.ObjectDict.ContainsKey("责任单位"))
+            {
+                try
+                {
+                    lbDutyUnits.Items.Clear();
+                    Newtonsoft.Json.Linq.JArray teams = (Newtonsoft.Json.Linq.JArray)MainConfig.Config.ObjectDict["责任单位"];
+                    foreach (string s in teams)
+                    {
+                        lbDutyUnits.Items.Add(s);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+    }
+}
